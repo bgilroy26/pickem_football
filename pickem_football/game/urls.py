b9958154eaa29dd588003987913f1c855cb8e0ed
Connor Hammond <ConnorH2582@users.noreply.github.com>
2015-06-09 14:33:27 -0400

    Creates static & template directories, urls.py file in game app.

--- pickem_football/game/urls.py
+++ pickem_football/game/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('',
     # url(r'^$', 'pickem_football.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^pickem/$', include('game.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+
 )
