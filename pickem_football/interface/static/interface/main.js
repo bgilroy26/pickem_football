20e08f96d6776d374902e4c275984b6ac2afcd4b
Connor Hammond <ConnorH2582@users.noreply.github.com>
2015-06-23 19:26:42 -0400

    Adds some JS that works; makes picks show up as selected radio button

--- pickem_football/interface/static/interface/main.js
+++ pickem_football/interface/static/interface/main.js
@@ -1,4 +1,4 @@
-// $(document).ready(function(){
+$(document).ready(function(){
 //     $(".team_list_link").on("click", function(event) {
 //         event.preventDefault();
 //         var userName = $('#target_user').children('a').text();
@@ -13,4 +13,28 @@
 //                 }
 //         });
 //     });
-// });
+    var current_picks_list = current_picks_data['picks'];
+    var initial_choices = [];
+
+
+    for (var i = 0; i < current_picks_list.length; i++) {
+        initial_choices.push(JSON.parse(current_picks_list[i])['choice']);
+    }
+
+    for (var i = 0; i < initial_choices.length; i++) {
+        console.log(initial_choices[i]);
+    }
+
+
+    inputArr = $('input');
+    $.each(inputArr, function(idx, inputEl){
+
+        if (initial_choices.indexOf(inputEl.value) > -1) {
+            inputEl.checked = true;
+            $('input[value="' + inputEl.value + '"]').prop('checked', true);
+        };
+    });
+    console.log("after func");
+
+
+});
